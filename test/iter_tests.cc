f0dae5e0d31a4a851bf24293172c19a99802a5f0
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-08-25 14:33:36 +0800

    Add first tests

---
+++ test/iter_tests.cc
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+namespace mana {
+  TEST(Tutor, IterTest) {
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    for (auto e : v) {
+      std::cout << e << std::endl;
+    }
+
+    for (auto iter = v.begin(); iter != v.end(); ++iter) {
+      std::cout << *iter << std::endl;
+    }
+  }
+}
