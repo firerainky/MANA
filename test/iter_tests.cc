dd3df1fd8c543126fa71ec2a64b96c0f110e16cd
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-08-25 15:03:12 +0800

    Add clang-format

--- test/iter_tests.cc
+++ test/iter_tests.cc
@@ -2,14 +2,10 @@
 #include <vector>
 
 namespace mana {
-  TEST(Tutor, IterTest) {
-    std::vector<int> v = {1, 2, 3, 4, 5};
-    for (auto e : v) {
-      std::cout << e << std::endl;
-    }
+    TEST(Tutor, IterTest) {
+        std::vector<int> v = {1, 2, 3, 4, 5};
+        for (auto e: v) { std::cout << e << std::endl; }
 
-    for (auto iter = v.begin(); iter != v.end(); ++iter) {
-      std::cout << *iter << std::endl;
+        for (auto iter = v.begin(); iter != v.end(); ++iter) { std::cout << *iter << std::endl; }
     }
-  }
-}
+}// namespace mana
