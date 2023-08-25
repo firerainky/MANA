848a3500996a0bdb3d5e954b0c105c29a0c79fd3
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-08-25 11:44:05 +0800

    Add reverse vector and cast to char

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+
+    std::vector<char> v;
+    v.push_back('a');
+
+    std::string s;
+
+    for (auto i = v.rbegin(); i != v.rend(); ++i)
+    {
+        s += (char)(*i + '0');
+        cout << *i << endl;
+    }
+
     cout << "Hello World!" << endl;
     return 0;
 }
