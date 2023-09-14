632bc68302d218127adb88ef0725aeebe3d3b7ce
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-09-14 17:34:52 +0800

    Added smart pointer

--- geek_bang_cpp/012_ShapeWrapper/shape_wrapper.cc
+++ geek_bang_cpp/012_ShapeWrapper/shape_wrapper.cc
@@ -12,21 +12,25 @@ enum class shape_type {
 class Shape {
 public:
     virtual ~Shape(){};
+    virtual void draw(){};
 };
 class Circle : public Shape {
 public:
     Circle() { puts("Shape::Circle()"); }
     ~Circle() { puts("~Shape::Circle()"); }
+    void draw() { puts("Draw Shape::Circle()"); }
 };
 class Triangle : public Shape {
 public:
     Triangle() { puts("Shape::Triangle()"); }
     ~Triangle() { puts("~Shape::Triangle()"); }
+    void draw() { puts("Draw Shape::Triangle()"); }
 };
 class Rectangle : public Shape {
 public:
     Rectangle() { puts("Shape::Rectangle()"); }
     ~Rectangle() { puts("~Shape::Rectangle()"); }
+    void draw() { puts("Draw Shape::Rectangle()"); }
 };
 
 inline Shape *create_shape(shape_type type) {
@@ -53,14 +57,37 @@ private:
 
 void foo() { ShapeWrapper ptr_wrapper(create_shape(shape_type::Circle)); }
 
+template<typename T>
+class smart_ptr {
+public:
+    explicit smart_ptr(T *ptr = nullptr) : ptr_(ptr) {}
+    ~smart_ptr() { delete ptr_; }
+    T *get() const { return ptr_; }
+    T &operator*() const { return *ptr_; }
+    T *operator->() const { return ptr_; }
+
+    operator bool() const { return ptr_; }
+
+private:
+    T *ptr_;
+};
+
 int main() {
     // The rect destructor is not called when the program ends.
     // This is a memory leak caused by "object slicing"
     Shape *rect = create_shape(shape_type::Rectangle);
+    rect->draw();
 
     // Using ShapeWrapper will not cause such problem
     // because ShapeWrapper is not allocated on heap but allocated on stack.
     // Then RAII is working to call destructor of Shape
     ShapeWrapper ptr_wrapper(create_shape(shape_type::Triangle));
+    ptr_wrapper.get()->draw();
     foo();
+
+    // smart_ptr<Shape> smartP(new Rectangle());
+    smart_ptr<Shape> smartP(create_shape(shape_type::Rectangle));
+    if (smartP) { puts("Good yep"); }
+    smartP->draw();
+    (*smartP).draw();
 }
