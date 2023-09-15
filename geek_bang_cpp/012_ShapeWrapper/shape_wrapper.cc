b82c49f59bf110d61bee1ad854b91b93ad2599c3
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-09-15 11:01:48 +0800

    Added unique_ptr

--- geek_bang_cpp/012_ShapeWrapper/shape_wrapper.cc
+++ geek_bang_cpp/012_ShapeWrapper/shape_wrapper.cc
@@ -62,6 +62,27 @@ class smart_ptr {
 public:
     explicit smart_ptr(T *ptr = nullptr) : ptr_(ptr) {}
     ~smart_ptr() { delete ptr_; }
+
+    // Disable copy constructor and assign operator
+    // smart_ptr(const smart_ptr &) = delete;
+    // smart_ptr &operator=(const smart_ptr &) = delete;
+
+    template<typename U>
+    smart_ptr(smart_ptr<U> &&other) {
+        ptr_ = other.release();
+    }
+    smart_ptr(smart_ptr &&other) { ptr_ = other.release(); }
+    smart_ptr &operator=(smart_ptr rhs) {
+        rhs.swap(*this);
+        return *this;
+    }
+
+    T *release() {
+        T *ptr = ptr_;
+        ptr_ = nullptr;
+        return ptr;
+    }
+
     T *get() const { return ptr_; }
     T &operator*() const { return *ptr_; }
     T *operator->() const { return ptr_; }
@@ -90,4 +111,8 @@ int main() {
     if (smartP) { puts("Good yep"); }
     smartP->draw();
     (*smartP).draw();
+
+    smart_ptr<Shape> ptr1{create_shape(shape_type::Circle)};
+    // smart_ptr<Shape> ptr2{ptr1};
+    smart_ptr<Shape> ptr3 = std::move(ptr1);
 }
